f41f5534816284a35a102919c8b539c49bc56658
Vipin Sharma <sh.vipin@gmail.com>
2020-05-06 11:21:40 -0700

    axon: Add support for packet capture in AXON
    Change includes:
       - App for running tcpdump
       - Improvements in Console app.
       - Unit test cases for TCPDump app.
       - Add missing license header in files.
    
    Signed-off-by: Vipin Sharma <sh.vipin@gmail.com>

--- axon/apps/console.py
+++ axon/apps/console.py
@@ -7,11 +7,12 @@
 This module defines Console App in AXON system.
 
 This app shall allow running the commands on the endpoints locally. Other apps
-which need to work on console such as Running traffic through other tools 
-(Iperf etc) or running port monioring tools shall also use/inherit this common app.
+which need to work on console such as Running traffic through other tools
+(Iperf etc) or running port monioring tools shall also use/inherit this common
+app.
 
-NOTE: This should not be exposed via controller as it can pause security risk as
-remote endpoints can run malicious commands using the exposed interface.
+NOTE: This should not be exposed via controller as it can pause security risk
+as remote endpoints can run malicious commands using the exposed interface.
 """
 
 import logging
@@ -57,25 +58,51 @@ class Console(BaseApp):
         (int, string)
             returns a tuple of command execution return code and output.
         """
-        cmnd = shlex.split(cmnd)
-        p = subprocess.Popen(cmnd, shell=False, stdin=None, bufsize=-1, env=env,
-                             stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
-                             close_fds=True, cwd=cwd, preexec_fn=os.setsid)
+
+        p = self._start_subprocess(cmnd=cmnd, cwd=cwd, env=env)
 
         # Start the counter for command to finish if requested.
         if timeout > 0:
             time_limit = time.time() + timeout
 
             while time.time() < time_limit:
-                if p.poll() is None:
+                if self._is_alive(p):
                     time.sleep(1)
                 else:
                     break
-
-            # command is still active, kill it.
-            if p.poll() is None:
-                log.warning("TIMEOUT : Killing command %s" % cmnd)
-                os.killpg(os.getpgid(p.pid), signal.SIGTERM)
+            self._kill_subprocess(p)
 
         stdout_val = p.communicate()[0]
         return p.returncode, stdout_val.strip().decode('utf-8')
+
+    def _start_subprocess(self, cmnd,
+                          stdin=None, stdout=None, stderr=None,
+                          env=None, cwd=None):
+        """
+        Starts a subprocess and returns a handle for it.
+        """
+        stdout = subprocess.PIPE if not stdout else stdout
+        stderr = subprocess.STDOUT if not stderr else stderr
+
+        cmnd = shlex.split(cmnd)
+
+        return subprocess.Popen(cmnd, shell=False, cwd=cwd, env=env,
+                                stdin=stdin, stdout=stdout, stderr=stderr,
+                                close_fds=True, preexec_fn=os.setsid,
+                                bufsize=-1)
+
+    def _kill_subprocess(self, proc, close_fds=False):
+        """
+        Kills a subprocess represented by handle 'proc'.
+        """
+        # command is still active, kill it.
+        log.warning("Killing subprocess %s" % proc.pid)
+        proc.kill()
+        if close_fds:
+            proc.communicate()
+
+    def _is_alive(self, proc):
+        """
+        Returns True if subprocess is still running else False
+        """
+        return proc.poll() is None
