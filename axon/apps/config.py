4782a812c49747b8550d7aef2216a02d3898652c
Gavin Chang <cgavin@vmware.com>
2020-09-28 16:47:55 -0700

    [Axon]: Extend support for config app
    Moving all config variables from axon/common/config.py to config App.
    
    - Store config variables along with their types to DB
    - Retrieve the values of variables from DB with the same type as they configured in config.py.
    - Remove axon/common/config.py and create a symlink to point axon/apps/config.py for backward compatibility.
    Tests
    -----
    >>> from axon.apps import config
    >>> config.get_param('LINUX_OS')
    True
    >>> type(config.get_param('LINUX_OS'))
    <class 'bool'>
    >>> config.get_param('NAMESPACE_INTERFACE_NAME_PREFIXES')
    ['veth', 'eth']
    >>> type(config.get_param('NAMESPACE_INTERFACE_NAME_PREFIXES'))
    <class 'list'>
    >>> config.get_param('LOG_DIR')
    '/var/log/axon'
    >>> type(config.get_param('LOG_DIR'))
    <class 'str'>
    >>>
    >>> config.get_param('PACKET_SIZE')
    1024
    >>> type(config.get_param('PACKET_SIZE'))
    <class 'int'>
    >>> config.get_param('WAVEFRONT_REPORT_PERC')
    1.0
    >>> type(config.get_param('WAVEFRONT_REPORT_PERC'))
    <class 'float'>
    
     -----------
    |DB snippet:|
     -----------
    sqlite> select * from config;
    ALLOW_REUSE_ADDRESS|True|bool
    AXON_PORT|5678|int
    ELASTIC_SEARCH_SERVER_ADDRESS|None|NoneType
    ELASTIC_SEARCH_SERVER_PORT|9200|int
    LINUX_OS|True|bool
    LOG_DIR|/var/log/axon|str
    LOG_FILE|axon.log|str
    NAMESPACE_INTERFACE_NAME_PREFIXES|['veth', 'eth']|list
    NAMESPACE_MODE|False|bool
    PACKET_SIZE|1024|int
    RECORDER|None|NoneType
    RECORD_COUNT_UPDATER_SLEEP_INTERVAL|30|int
    RECORD_UPDATER_THREAD_POOL_SIZE|50|int
    REQUEST_QUEUE_SIZE|100|int
    TESTBED_NAME|None|NoneType
    TEST_ID|None|NoneType
    WAVEFRONT_PROXY_ADDRESS|None|NoneType
    WAVEFRONT_REPORT_PERC|1.0|float
    WAVEFRONT_SERVER_ADDRESS|None|NoneType
    WAVEFRONT_SERVER_API_TOKEN|None|NoneType
    WAVEFRONT_SOURCE_TAG|systest-runner|str
    Issue: #
    Jira: #NPTF-2328
    Change-Id: Ib1c32650e4f9680e6b3388c3c83f2db757616a3b

--- axon/apps/config.py
+++ axon/apps/config.py
@@ -4,21 +4,75 @@
 # The full license information can be found in LICENSE.txt
 # in the root directory of this project.
 
+import json
 import logging
 import os
+import platform
+import socket
+import sys
+
 from sql30 import db
 
-
 from axon.apps.base import BaseApp
+from axon.common import consts, utils
 
 log = logging.getLogger(__name__)
 configs = None
+
+
+# # # # # All Configurable Variables set below # # # # #
+
+LINUX_OS = "Linux" in platform.uname()
+LOG_FILE = os.environ.get('LOG_FILE', consts.LOG_FILE)
+LOG_DIR = os.environ.get('LOG_DIR', consts.LOG_DIR)
+utils.setup_logging(log_dir=LOG_DIR, log_file=LOG_FILE)
+
+
+# Traffic Server Configs
+REQUEST_QUEUE_SIZE = 100
+PACKET_SIZE = 1024
+ALLOW_REUSE_ADDRESS = True
+
+
+# Env Configs
+TEST_ID = os.environ.get('TEST_ID', None)
+TESTBED_NAME = os.environ.get('TESTBED_NAME', None)
+AXON_PORT = int(os.environ.get('AXON_PORT', 5678))
+
+
+# Wavefront recorder configs
+WAVEFRONT_PROXY_ADDRESS = os.environ.get('WAVEFRONT_PROXY_ADDRESS', None)
+WAVEFRONT_SERVER_ADDRESS = os.environ.get('WAVEFRONT_SERVER_ADDRESS', None)
+WAVEFRONT_SERVER_API_TOKEN = os.environ.get('WAVEFRONT_SERVER_API_TOKEN', None)
+WAVEFRONT_SOURCE_TAG = os.environ.get('WAVEFRONT_SOURCE', socket.gethostname())
+WAVEFRONT_REPORT_PERC = float(os.environ.get('WAVEFRONT_REPORT_PERC', 1.0))
+
+
+# Namespace Configs
+NAMESPACE_MODE = os.environ.get("NAMESPACE_MODE", False)
+NAMESPACE_MODE = True if NAMESPACE_MODE in ['True', True] else False
+NAMESPACE_INTERFACE_NAME_PREFIXES = ["veth", "eth"]
+
+
+# Recorder Configs
+RECORDER = os.environ.get('RECORDER', None)
+RECORD_COUNT_UPDATER_SLEEP_INTERVAL = 30
+RECORD_UPDATER_THREAD_POOL_SIZE = 50
+
+ELASTIC_SEARCH_SERVER_ADDRESS = os.environ.get('ELASTIC_SEARCH_SERVER_ADDRESS', None)
+ELASTIC_SEARCH_SERVER_PORT = os.environ.get(
+    'ELASTIC_SEARCH_SERVER_PORT', consts.ELASTIC_SEARCH_PORT)
+
+# # # # # End of Configurable Variables  # # # # #
 
 
 class Config(db.Model, BaseApp):
     NAME = "CONFIG"
     DB_NAME = 'config.db'
     TABLE = 'config'
+    SINGLE_QUOTE = '\''
+    DOUBLE_QUOTE = '\"'
+
     DB_SCHEMA = {
         'db_name': DB_NAME,
         'tables': [
@@ -27,13 +81,12 @@ class Config(db.Model, BaseApp):
                 'fields': {
                     'param': 'text',
                     'value': 'text',
+                    'typename': 'text',
                     },
                 'primary_key': 'param'  # avoid duplicate entries.
             }]
         }
     VALIDATE_BEFORE_WRITE = True
-
-    DEFAULT_CONFIG = '/etc/axon/axon.conf'
 
     def __init__(self, db_file=None):
         # Set database name.
@@ -45,36 +98,56 @@ class Config(db.Model, BaseApp):
         self._params = {}
 
         # Read configs, load from db file.
+
         self._read_config()
         self.load_from_db()
         self.save_to_db()
 
     def _read_config(self):
         """
-        Reads config from default config file for the service.
+        Reads config from default config file.
 
         We do not write these configs into the database yet as database
-        configs are supposd to overwrite.
+        configs are supposed to overwrite.
         """
-        configs = []
-        if os.path.exists(self.DEFAULT_CONFIG):
-            with open(self.DEFAULT_CONFIG, 'r') as fp:
-                configs = fp.readlines()
 
-        for config in configs:
-            config = config.strip()
-            if config.startswith('#'):
-                continue
-            param, val = config.split('=')
-            self._params[param] = val   # initialize values.
+        module_name = sys.modules[__name__]
+        VARS = [var for var in dir(module_name) if var.isupper()]
+        for var in VARS:
+            param, val = var, getattr(module_name, var)
+            self._params[param] = val
+
+    def _type_handler(self, val, type_name):
+
+        types_map = {'str': lambda x: x,
+                     'int': lambda x: int(x),
+                     'float': lambda x: float(x),
+                     'bool': lambda x: True if x == 'True' else False,
+                     'list': lambda x: json.loads(x.replace(self.SINGLE_QUOTE,
+                                                            self.DOUBLE_QUOTE)),
+                     'dict': lambda x: json.loads(x.replace(self.SINGLE_QUOTE,
+                                                            self.DOUBLE_QUOTE)),
+                     'NoneType': lambda x: None,
+                     }
+
+        if type_name in types_map:
+            return types_map[type_name](val)
+        elif type_name == 'tuple':
+            val = val.replace('(', '[').replace(')', ']')
+            return tuple(types_map['list'](val))
+        elif type_name == 'set':
+            s_val = val.replace('{', '[').replace('}', ']')
+            return set(types_map['list'](s_val))
+        else:
+            return val
 
     def load_from_db(self):
         """
         Load config params from database file to local cache.
         """
         configs = self.read()
-        for key, val in configs:
-            self._params[key] = val
+        for key, val, type_name in configs:
+            self._params[key] = self._type_handler(val, type_name)
 
     def save_to_db(self):
         """
@@ -104,10 +177,11 @@ class Config(db.Model, BaseApp):
         Sets a param, val in database file.
         """
         record = self.read(param=param)
+        type_name = type(val).__name__
         if record:
-            self.update(condition={'param':param}, value=val)
+            self.update(condition={'param': param}, value=str(val), typename=type_name)
         else:
-            self.write(param=param, value=val)
+            self.write(param=param, value=str(val), typename=type_name)
 
 
 def _get_configs():
